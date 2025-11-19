95657204721dbccaf14683f07cf6959a214bc8c1
jeferson020 <j.picado.3@estudiante.cr>
2025-11-19 12:25:14 -0600

    eliminacion de çpulga en problem detail

--- gui_codecoach/components/ProblemDetailWidget.cpp
+++ gui_codecoach/components/ProblemDetailWidget.cpp
@@ -1,11 +1,17 @@
 #include "ProblemDetailWidget.h"
-using cc::dto::ProblemDetail;
+#include <QString>
 
-ProblemDetailWidget::ProblemDetailWidget(QWidget* parent) : QTextBrowser(parent) {}
+ProblemDetailWidget::ProblemDetailWidget(QWidget* parent)
+    : QTextBrowser(parent)
+{
+}
 
-void ProblemDetailWidget::render(const ProblemDetail& d) {
+void ProblemDetailWidget::render(const cc::dto::ProblemDetail& d) {
     QString ex;
-    for (const auto& e : d.examples) ex += "<pre>" + e.toHtmlEscaped() + "</pre>";
+    for (const auto& e : d.examples) {
+        ex += "<pre>" + e.toHtmlEscaped() + "</pre>";
+    }
+
     QString tg = d.tags.join(", ");
 
     setHtml(QString(
@@ -13,5 +19,5 @@ void ProblemDetailWidget::render(const ProblemDetail& d) {
         "<br><b>Tags:</b> %3</p>%4"
         "<h3>Enunciado</h3>%5")
         .arg(d.title, d.difficulty, tg, ex, d.statementHtml));
-}
+};
 
--- gui_codecoach/components/ProblemDetailWidget.h
+++ gui_codecoach/components/ProblemDetailWidget.h
@@ -1,24 +1,21 @@
-//
-// Created by andres on 6/10/25.
-//
+
 
 #ifndef GUI_CODECOACH_PROBLEMDETAILWIDGET_H
 #define GUI_CODECOACH_PROBLEMDETAILWIDGET_H
 
-#pragma once
+#include <QWidget>
 #include <QTextBrowser>
 #include "../viewmodels/dto/ProblemDetail.h"
 
 class ProblemDetailWidget : public QTextBrowser {
-    Q_OBJECT
-public:
-    explicit ProblemDetailWidget(QWidget* parent=nullptr);
+    Q_OBJECT;
 
-public slots:
+public:
+    explicit ProblemDetailWidget(QWidget* parent = nullptr);
+
+public:
     void render(const cc::dto::ProblemDetail& d);
 };
 
+#endif // GUI_CODECOACH_PROBLEMDETAILWIDGET_H
 
-
-#endif //GUI_CODECOACH_PROBLEMDETAILWIDGET_H
-// lista de prblemas
