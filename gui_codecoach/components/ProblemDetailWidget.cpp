57f0ec7f5ebd535f293d705596a14c731866858c
Andrés Campos <andres220204ca@gmail.com>
2025-11-30 14:25:07 -0600

    actualizacion en el detailwidget

--- gui_codecoach/components/ProblemDetailWidget.cpp
+++ gui_codecoach/components/ProblemDetailWidget.cpp
@@ -7,17 +7,29 @@ ProblemDetailWidget::ProblemDetailWidget(QWidget* parent)
 }
 
 void ProblemDetailWidget::render(const cc::dto::ProblemDetail& d) {
+    // bloque de ejemplos
     QString ex;
     for (const auto& e : d.examples) {
         ex += "<pre>" + e.toHtmlEscaped() + "</pre>";
     }
 
+    // Tags
     QString tg = d.tags.join(", ");
 
-    setHtml(QString(
-        "<h2>%1</h2><p><b>Dificultad:</b> %2"
-        "<br><b>Tags:</b> %3</p>%4"
-        "<h3>Enunciado</h3>%5")
-        .arg(d.title, d.difficulty, tg, ex, d.statementHtml));
-};
 
+    QString html = QString(
+        "<h2>%1</h2>"
+        "<p><b>Dificultad:</b> %2"
+        "<br><b>Tags:</b> %3</p>"
+        "%4"
+        "<h3>Enunciado</h3>%5"
+    ).arg(
+        d.title.toHtmlEscaped(),       // %1
+        d.difficulty.toHtmlEscaped(),  // %2
+        tg.toHtmlEscaped(),            // %3
+        ex,                            // %4 (ya tiene <pre>…</pre>)
+        d.statementHtml                // %5 (HTML ya preparado)
+    );
+
+    setHtml(html);
+}
--- gui_codecoach/components/ProblemDetailWidget.h
+++ gui_codecoach/components/ProblemDetailWidget.h
@@ -3,19 +3,16 @@
 #ifndef GUI_CODECOACH_PROBLEMDETAILWIDGET_H
 #define GUI_CODECOACH_PROBLEMDETAILWIDGET_H
 
-#include <QWidget>
 #include <QTextBrowser>
 #include "../viewmodels/dto/ProblemDetail.h"
 
 class ProblemDetailWidget : public QTextBrowser {
-    Q_OBJECT;
+    Q_OBJECT
 
 public:
     explicit ProblemDetailWidget(QWidget* parent = nullptr);
 
-public:
     void render(const cc::dto::ProblemDetail& d);
 };
 
 #endif // GUI_CODECOACH_PROBLEMDETAILWIDGET_H
-
